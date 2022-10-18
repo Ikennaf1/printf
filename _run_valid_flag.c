18614beb26b4b826e396e328ecaccb050f578d2c
Ikennaf1 <iiikbros202@gmail.com>
2022-10-18 22:00:24 +0100

    More betty clean ups

--- _run_valid_flag.c
+++ _run_valid_flag.c
@@ -3,6 +3,7 @@
 /**
  * _run_valid_flag - Runs the function for the given flag
  * @c: The flag
+ * @arg: The given arg
  * @wb: The buffer
  * Return: int or 0
  */
@@ -10,21 +11,12 @@ int _run_valid_flag(char c, va_list arg, word_buffer *wb)
 {
 	switch (c)
 	{
-		/*case '#':
-			_run_flag_hash(arg, wb);
-			break;*/
 		case ' ':
 			_run_flag_space(arg, wb);
 			break;
 		case '+':
 			_run_flag_plus(arg, wb);
 			break;
-		/*case '0':
-			_run_flag_zero(arg, wb);
-			break;
-		case '-':
-			_run_flag_left_justify(arg, wb);
-			break;*/
 	}
 	return (0);
 }
